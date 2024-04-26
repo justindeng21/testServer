e67216144b80c6cf1fe75220668221de00807f40
justindeng21 <justin.n.deng@gmail.com>
2024-04-26 11:24:26 -0600

    auto-deploy

--- evidon.js
+++
@@ -1,9 +0,0 @@
-(function append() {
-    let d = document, sn = "script", f = d.getElementsByTagName(sn)[0];
-    if (!f) f = d.head;
-    let s = d.createElement(sn);
-    s.src ="https://c.evidon.com/dg/dg.js";
-    s.charset = "utf-8";
-    s.setAttribute("companyid","3714");
-    f.parentNode.insertBefore(s, f);
-})()
