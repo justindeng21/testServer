de408de90ff36a513a9745cc56792562392659a1
justindeng21 <justin.n.deng@gmail.com>
2024-06-14 12:03:44 -0600

    auto-deploy

--- dist/js/ucpStub.js
+++ dist/js/ucpStub.js
@@ -7,7 +7,7 @@ const config = { attributes: true, childList: true, subtree: true };
 // Callback function to execute when mutations are observed
 const callback = (mutationList, observer) => {
     for (const mutation of mutationList) {
-        if(mutation.addedNodes.length !== 0):
+        if(mutation.addedNodes.length !== 0)
             console.log(mutation.addedNodes[0].id)
     }
 };
