f1d072cf5435de0a2e4afbc5c7616a0807ba920d
justindeng21 <justin.n.deng@gmail.com>
2024-06-14 11:36:42 -0600

    auto-deploy

--- dist/js/ucpStub.js
+++ dist/js/ucpStub.js
@@ -7,7 +7,11 @@ const config = { attributes: true, childList: true, subtree: true };
 // Callback function to execute when mutations are observed
 const callback = (mutationList, observer) => {
   for (const mutation of mutationList) {
-      console.log(mutation);
+    if (mutation.type === "childList") {
+      console.log("A child node has been added or removed.");
+    } else if (mutation.type === "attributes") {
+      console.log(`The ${mutation.attributeName} attribute was modified.`);
+    }
   }
 };
 
