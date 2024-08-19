e5e89fad07d9c3afc4973ec1a241ac60b693599e
justindeng21 <justin.n.deng@gmail.com>
2024-08-19 13:46:47 -0600

    auto-deploy

--- dist/js/debugMode.js
+++ dist/js/debugMode.js
@@ -1,3 +1,5 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
 gtag('policy', 'inject_script', function(containerId, permissionId, data) {
 
     let url = data.url || '';
