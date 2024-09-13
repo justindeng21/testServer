098939f67dadc83abd48e51f8af028a1edf2e959
justindeng21 <justin.n.deng@gmail.com>
2024-09-13 12:57:11 -0600

    auto-deploy

--- dist/js/debug.js
+++ dist/js/debug.js
@@ -1,3 +1,10 @@
+let defaultConsentIndex = -1;
+
 for(let i in dataLayer){
-    if(dataLayer[i].value != undefined) console.log(dataLayer[i].value.event);
+    if(dataLayer[i].value !== undefined) 
+        if (dataLayer[i].value.event === 'evidonDefaultConsent'){
+            defaultConsentKey = i
+        } 
 }
+
+
