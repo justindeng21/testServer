f044ec9ccb00e0c3c1289c7b3921ef5c139bcbd7
justindeng21 <justin.n.deng@gmail.com>
2024-09-18 15:31:03 -0600

    auto-deploy

--- dist/js/debug.js
+++ dist/js/debug.js
@@ -1,10 +1,5 @@
-let defaultConsentIndex = -1;
-
-for(let i in dataLayer){
-    if(dataLayer[i].value !== undefined) 
-        if (dataLayer[i].value.event === 'evidonDefaultConsent'){
-            defaultConsentKey = i
-        } 
+if(window.google_tag_data){
+    console.log("Implementation is incorrect");
 }
 
 
