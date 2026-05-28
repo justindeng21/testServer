5ce4389f2bfa03f8f8fd8c0dfcf297c20ff03a81
Justin <justin.n.deng@gmail.com>
2026-05-28 11:33:21 -0600

    auto-deploy

---
+++ dist/js/gm.js
@@ -0,0 +1,43 @@
+
+(function(win, doc, style, timeout) {
+  var STYLE_ID = 'at-body-style';
+
+  function getParent() {
+    return doc.getElementsByTagName('head')[0];
+  }
+
+  function addStyle(parent, id, def) {
+    if (!parent) {
+      return;
+    }
+
+    var style = doc.createElement('style');
+    style.id = id;
+    style.innerHTML = def;
+    parent.appendChild(style);
+  }
+
+  function removeStyle(parent, id) {
+    if (!parent) {
+      return;
+    }
+
+    var style = doc.getElementById(id);
+
+    if (!style) {
+      return;
+    }
+
+    parent.removeChild(style);
+  }
+
+  addStyle(getParent(), STYLE_ID, style);
+  setTimeout(function() {
+    removeStyle(getParent(), STYLE_ID);
+  }, timeout);
+}(window, document, "body {opacity: 0 !important}", 3000));
+
+var script = document.createElement('script');
+script.src = 'https://assets.adobedtm.com/ea8c57fea068/1491e9d4ff4d/launch-9468ef50bc78-development.min.js';
+script.async = true;
+document.head.appendChild(script);
