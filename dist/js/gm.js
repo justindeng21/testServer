6548147dc7cbe64a44e0ff8a9ab7f047be0b2925
Justin <justin.n.deng@gmail.com>
2026-05-29 10:04:11 -0600

    auto-deploy

--- dist/js/gm.js
+++ dist/js/gm.js
@@ -1,5 +1,30 @@
 
+var script = document.createElement('script');
+script.src = 'https://assets.adobedtm.com/ea8c57fea068/1491e9d4ff4d/launch-9468ef50bc78-development.min.js';
+script.async = true;
 
+document.head.appendChild(script);
+
+document.addEventListener('click', function (event) {
+
+  var button = event.target.closest('#_evidon-accept-button');
+
+  if (button) {
+
+    // Prevent infinite reload loop
+    if (!sessionStorage.getItem('evidonReloaded')) {
+
+      sessionStorage.setItem('evidonReloaded', 'true');
+
+      setTimeout(function () {
+        window.location.reload();
+      }, 100);
+
+    }
+
+  }
+
+});
 var script = document.createElement('script');
 script.src = 'https://assets.adobedtm.com/ea8c57fea068/1491e9d4ff4d/launch-9468ef50bc78-development.min.js';
 script.async = true;
